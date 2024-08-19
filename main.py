d95e156badf9896cbe8a5ee62aec81c55fa0430e
vladislav <vl_evlashin@mail.ru>
2024-08-20 01:27:29 +0300

    add some changes

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = ''
-page = requests.get(url)
-soup = BeautifulSoup(page.content, "html.parser")
+url = 'https://www.avito.ru/moskva_i_mo/avtomobili/mercedes-benz-ASgBAgICAUTgtg3omCg?cd=1&p='
 
-print(soup)
+results = []
+
+for i in range(1, 3):
+    print(i)
+    page = requests.get(f'{url}{i}')
+    soup = BeautifulSoup(page.content, "html.parser")
+    allItems = soup.findAll('div', attrs={'data-marker': 'item'})
+
+    for item in allItems:
+        name = item.find(attrs={'itemprop': 'name'}).text
+        price = item.find(attrs={'itemprop': 'price'}).attrs['content']
+        item_obj = {
+            'name': name,
+            'price': price
+        }
+        results.append(item_obj)
+
+file = open("data.json", "w")
+file.write(f'{results}')
+file.close()
+
+print(results)
